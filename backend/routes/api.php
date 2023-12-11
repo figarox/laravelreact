a48b4937fe8a54276f762ac9d308300d2eca5088
Maciej Rożnawski <intro202@wp.pl>
2023-12-11 14:19:22 +0000

    next commit

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\SurveyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/signup' , [AuthController::class, 'signup']);
+Route::post('/login' , [AuthController::class, 'login']);
+
